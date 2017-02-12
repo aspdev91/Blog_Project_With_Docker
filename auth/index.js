493592539d6f827d0722381f574daa1f78415182
aspdev91 <mtgmusic1@gmail.com>
2017-02-12 15:37:49 -0500

    minor declaration changes and limit on all blog retrieval

--- auth/index.js
+++ auth/index.js
@@ -1,30 +1,30 @@
-var base64url = require('base64url')
-var JWT = require('../utils/jwt')
-var KeyService = require('../services/keyservice')
+const base64url = require('base64url')
+const JWT = require('../utils/jwt')
+const KeyService = require('../services/keyservice')
 
 function isAuthenticated(req, res, next) {
  
-  var authorization = req.headers.authorization
+  let authorization = req.headers.authorization
   if (!authorization || !(authorization.search('Bearer ') === 0)) {
     return next(new Error('401 Missing Authorization Header'))
   }
-  var token = authorization.split(' ')[1]
+  let token = authorization.split(' ')[1]
   if (!token) {
     return next(new Error('401 Missing Bearer Token'))
   }
 
   // Unpack JWT
-  var components = token.split('.')
-  var header = JSON.parse(base64url.decode(components[0]))
-  var payload = JSON.parse(base64url.decode(components[1]))
-  var signature = components[2]
+  let components = token.split('.')
+  let header = JSON.parse(base64url.decode(components[0]))
+  let payload = JSON.parse(base64url.decode(components[1]))
+  let signature = components[2]
 
   req.param.email = payload.email
 
   // Verify JWT
   KeyService.get(payload.jti)
     .then(function(userKey) {
-      var authenticated = JWT.verify(token, userKey)
+      let authenticated = JWT.verify(token, userKey)
       if (authenticated) {
         return next()
       }
